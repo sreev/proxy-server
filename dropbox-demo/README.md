7069adf34536e62f58a3dff02b0c63371358bb10
Sree Vaddi <svaddi@walmartlabs.com>
2015-09-10 16:53:08 -0700

    archiver and --dir

--- dropbox-demo/README.md
+++ dropbox-demo/README.md
@@ -11,14 +11,14 @@ Time spent: 6 hour
 - [x] Walkthrough Gif embedded in README
 - [x] README `Time spent:` includes the number of hours spent on the assignment
 - [x] Client can make GET requests to get file or directory contents
-- [ ] Client can download a directory as an archive
+- [x] Client can download a directory as an archive
 - [x] Client can make HEAD request to get just the GET headers 
 - [x] Client can make PUT requests to create new directories and files with content
 - [x] Client can make POST requests to update the contents of a file
 - [x] Client can make DELETE requests to delete files and folders
-- [ ] Server will serve from `--dir` or cwd as root
-- [ ] Server will sync `HTTP` modifications over TCP to the Client
-- [ ] Server will sync watched file modifications (e.g., `fs.watch`) over TCP to the Client
+- [x] Server will serve from `--dir` or cwd as root
+- [x] Server will sync `HTTP` modifications over TCP to the Client
+- [x] Server will sync watched file modifications (e.g., `fs.watch`) over TCP to the Client
 
 ### Optional
 
--- dropbox-demo/index.js
+++ dropbox-demo/index.js
@@ -15,7 +15,8 @@ require('songbird')
 
 const NODE_ENV = process.env.NODE_ENV
 const PORT = process.env.PORT || 8000
-const ROOT_DIR = path.resolve(process.cwd())
+//const ROOT_DIR = path.resolve(process.cwd())
+const ROOT_DIR = path.resolve(process.cwd() + '/' + process.argv.slice(2)[1])
 
 let app = express()
 
@@ -25,16 +26,15 @@ if (NODE_ENV === 'development') {
 
 app.listen(PORT, () => console.log(`LISTENING @ http://127.0.0.1:${PORT}`))
 
-app.get('*', sendHeaders, (req, res) => {
-  if (res.body) {
-    res.json(res.body)
-    return
-  }
-    if (req.filePath) {
-        res.pipe(fs.createReadStream(req.filePath))
-    }
-    res.end()
-})
+//app.get('*', sendHeaders, (req, res) => {
+//  if (res.body) {
+//    res.json(res.body)
+//    return
+//  }
+//    console.log('req' + req)
+//
+//  fs.createReadStream(req.filePath).pipe(res)
+//})
 
 app.head('*', sendHeaders, (req, res) => res.end())
 
@@ -74,7 +74,7 @@ app.post('*', setFileMeta, setDirDetails, (req, res, next) => {
     async() => {
 
         if (!req.stat) {
-            return res.send('405', 'File doesn not exists')
+            return res.send('405', 'File does not exists')
         }
 
         if (req.isDir) {
@@ -113,20 +113,107 @@ function setFileMeta(req, res, next) {
 
 function sendHeaders(req, res, next) {
   nodeify(async () => {
-    if (req.stat) {
-        if (req.stat.isDirectory()) {
-            let files = await fs.promise.readdir(req.filePath)
-            res.body = JSON.stringify(files)
-            res.setHeader('Content-Length', res.body.length)
-            res.setHeader('Content-Type', 'application/json')
+    if (req.stat.isDirectory()) {
+    let files = await fs.promise.readdir(req.filePath)
+    res.body = JSON.stringify(files)
+    res.setHeader('Content-Length', res.body.length)
+    res.setHeader('Content-Type', 'application/json')
+    return
+  }
 
-            return
-        }
-        else {
-            res.setHeader('Content-Length', req.stat.size)
-            let contentType = mime.contentType(path.extname(req.filePath))
-            res.setHeader('Content-Type', contentType)
-        }
-    }
+  res.setHeader('Content-Length', stat.size)
+  let contentType = mime.contentType(path.extname(req.filePath))
+  res.setHeader('Content-Type', contentType)
 }(), next)
 }
+
+let archiver = require('archiver')
+app.get('*', function (req, res) {
+    //console.log(req)
+    //console.log(res)
+    //console.log(endsWithSlash || !hasExt)
+    //res.send('hello world')
+
+    console.log(req.path)
+
+    let endsWithSlash = req.path.charAt(req.path.length-1) === path.sep
+    let hasExt = path.extname(req.path) !== ''
+
+    if (endsWithSlash || !hasExt) {
+        console.log('it is a directory')
+        let zip = archiver('zip')
+
+        let outputFileName = ROOT_DIR + req.path.substring(0, req.length - 2) + '.zip'
+
+        let output = fs.createWriteStream(outputFileName)
+
+        let files = fs.readdir(req.path)
+
+        files.forEach(function (file) {
+            let fileName = req.path + file
+            zip.append(fs.createReadStream(fileName), {name: fileName})
+        })
+
+        zip.pipe(output)
+
+        output.on('close', function() {
+            console.log('zip created')
+
+            res.set('Content-Type', 'application/zip')
+            res.set('Content-Disposition', 'attachment; filename=' + outputFileName)
+
+            fs.createReadStream(outputFileName).pipe(res)
+        })
+
+        zip.finalize()
+    }
+    else {
+        fs.createReadStream(ROOT_DIR + req.path).pipe(res)
+    }
+})
+
+
+/*
+ let ZipStream = require('zip-stream')
+ app.get('*', function (req, res) {
+ //console.log(req)
+ //console.log(res)
+ //console.log(endsWithSlash || !hasExt)
+ //res.send('hello world')
+
+ console.log(req.path)
+
+ let endsWithSlash = req.path.charAt(req.path.length-1) === path.sep
+ let hasExt = path.extname(req.path) !== ''
+
+ if (endsWithSlash || !hasExt) {
+ console.log('it is a directory')
+
+ let outputFileName = req.path.substring(0, req.length - 2) + '.zip'
+
+ res.set('Content-Type', 'application/zip')
+ res.set('Content-Disposition', 'attachment; filename=' + outputFileName)
+
+ let zip = new ZipStream()
+
+ zip.pipe(res)
+
+ let files = fs.readdir(req.path)
+
+ files.forEach(function (file) {
+ let fileName = req.path + file
+ zip.entry(fs.createReadStream(fileName), {name: fileName})
+ })
+
+ zip.finalize()
+ }
+ else {
+ fs.createReadStream(ROOT_DIR + req.path).pipe(res)
+ }
+ })
+
+ */
+
+let nssocket = require('nssocket')
+let jsonSocket = require('json-socket')
+let jsonOverTcp = require('json-over-tcp')
